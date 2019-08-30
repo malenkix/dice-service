925586f3220c1a5155e445139fed2c0e2fcd247f
Maik Thietke <seleenera@gmx.de>
2019-08-30 21:56:26 +0200

    Add tests

--- src/test/java/io/github/malenkix/diceservice/DiceServiceTest.java
+++ src/test/java/io/github/malenkix/diceservice/DiceServiceTest.java
@@ -1,6 +1,7 @@
 package io.github.malenkix.diceservice;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import java.util.Arrays;
 
@@ -20,5 +21,23 @@ public class DiceServiceTest {
   public void testRollSomeDicesShouldGiveResults() {
     final int[][] result = diceService.roll(2, 3, 20);
     assertEquals("[[6, 9, 8], [14, 15, 5]]", Arrays.deepToString(result));
+  }
+
+  @Test
+  public void testIllegalArgumentsShouldGiveError() {
+    assertIllegalRun(-1, 1, 2);
+    assertIllegalRun(1, -1, 2);
+    assertIllegalRun(1, 1, -1);
+    assertIllegalRun(0, 1, 2);
+    assertIllegalRun(1, 0, 2);
+    assertIllegalRun(1, 1, 1);
+  }
+
+  private void assertIllegalRun(int runs, int dices, int sides) {
+    try {
+      diceService.roll(runs, dices, sides);
+      fail(String.format("Expected arguments to be rejected. (rolls=%s, dices=%s, sides=%s)", runs, dices, sides));
+    } catch (IllegalArgumentException ex) {
+    }
   }
 }
